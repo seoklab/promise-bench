cec38c161cabc6d6eef91e05d8128e0ad7b35a2c
ProMiSE-bench <257918558+ProMiSE-bench@users.noreply.github.com>
2026-02-23 13:03:25 +0900

    feat(src): save high tm

--- src/curation/pipeline/cluster_by_tmscore.py
+++ src/curation/pipeline/cluster_by_tmscore.py
@@ -9,6 +9,7 @@ from sklearn.cluster import AgglomerativeClustering
 from tqdm import tqdm
 
 from ..utils.typedefs import TMScoreResult
+from ._data_root import DataRootCommand
 
 
 def cluster_by_tmscore(
@@ -42,33 +43,52 @@ def cluster_by_tmscore(
     # 1st pass: TM < 0.8
     mask1 = base_mask & (scoremap < cutoff) & (scoremap > 0)
 
-    # 2nd pass: 0.8 <= TM < 0.9 and RMSD > 3.0
-    mask2 = base_mask & (scoremap >= cutoff) & (scoremap < 0.9) & (rmsdmap > 3.0)
+    # 2nd pass: 0.8 < TM < 0.9 and RMSD > 3.0
+    mask2 = base_mask & (scoremap > cutoff) & (scoremap < 0.90) & (rmsdmap > 3.0)
 
-    mask = mask1 | mask2
-    x, y = np.nonzero(mask)
-    if len(x) == 0:
-        return None
+    x1, y1 = np.nonzero(mask1)
+    x2, y2 = np.nonzero(mask2)
 
-    # Ensure cluster-x < cluster-y
-    swap = cluster.labels_[x] > cluster.labels_[y]
-    x[swap], y[swap] = y[swap], x[swap]
+    df_main = None
+    df_high = None
 
-    df = pd.DataFrame.from_dict(
-        {
-            "center": tmscore_result.stem,
-            "cluster-x": cluster.labels_[x],
-            "cluster-y": cluster.labels_[y],
-            "chain-x": data.chains[x],
-            "chain-y": data.chains[y],
-            "TMscore": scoremap[x, y],
-            "RMSD": rmsdmap[x, y],
-        }
-    ).sort_values(["cluster-x", "cluster-y", "TMscore"])
-    return df
+    if len(x1) > 0:
+        # Ensure cluster-x < cluster-y
+        swap = cluster.labels_[x1] > cluster.labels_[y1]
+        x1[swap], y1[swap] = y1[swap], x1[swap]
+
+        df_main = pd.DataFrame.from_dict(
+            {
+                "center": tmscore_result.stem,
+                "cluster-x": cluster.labels_[x1],
+                "cluster-y": cluster.labels_[y1],
+                "chain-x": data.chains[x1],
+                "chain-y": data.chains[y1],
+                "TMscore": scoremap[x1, y1],
+                "RMSD": rmsdmap[x1, y1],
+            }
+        ).sort_values(["cluster-x", "cluster-y", "TMscore"])
+
+    if len(x2) > 0:
+        swap = cluster.labels_[x2] > cluster.labels_[y2]
+        x2[swap], y2[swap] = y2[swap], x2[swap]
+
+        df_high = pd.DataFrame.from_dict(
+            {
+                "center": tmscore_result.stem,
+                "cluster-x": cluster.labels_[x2],
+                "cluster-y": cluster.labels_[y2],
+                "chain-x": data.chains[x2],
+                "chain-y": data.chains[y2],
+                "TMscore": scoremap[x2, y2],
+                "RMSD": rmsdmap[x2, y2],
+            }
+        ).sort_values(["cluster-x", "cluster-y", "TMscore"])
+
+    return df_main, df_high
 
 
-@click.command()
+@click.command(cls=DataRootCommand)
 @click.option("--nproc", "-n", type=int, default=8)
 @click.option(
     "--clusters",
@@ -83,15 +103,21 @@ def cluster_by_tmscore(
     show_default=True,
 )
 @click.option(
+    "--high-tm",
+    type=click.Path(dir_okay=False, path_type=Path),
+    default=Path("data/high-tm-pairs.csv"),
+    show_default=True,
+)
+@click.option(
     "--scores",
     type=click.Path(exists=True, file_okay=False, path_type=Path),
     default=Path("data/scores"),
     show_default=True,
 )
-def main(nproc: int, clusters: Path, filtered: Path, scores: Path):
+def main(nproc: int, clusters: Path, filtered: Path, high_tm: Path, scores: Path):
     clusters.mkdir(exist_ok=True, parents=True)
 
-    dfs = Parallel(n_jobs=nproc)(
+    results = Parallel(n_jobs=nproc)(
         delayed(cluster_by_tmscore)(
             tm_result,
             clusters / tm_result.stem[1:3] / f"{tm_result.stem}.csv",
@@ -99,13 +125,17 @@ def main(nproc: int, clusters: Path, filtered: Path, scores: Path):
         for tm_result in tqdm(list(scores.rglob("*.npz")))
     )
 
+    dfs_main = [r[0] for r in results if r[0] is not None]
+    dfs_high = [r[1] for r in results if r[1] is not None]
+
     filtered.parent.mkdir(exist_ok=True, parents=True)
-    df = (
-        pd.concat(df for df in dfs if df is not None)
-        .reset_index(drop=True)
-        .sort_values("center")
-    )
-    df.to_csv(filtered, index=False)
+    if dfs_main:
+        df = pd.concat(dfs_main).reset_index(drop=True).sort_values("center")
+        df.to_csv(filtered, index=False)
+
+    if dfs_high:
+        df_high = pd.concat(dfs_high).reset_index(drop=True).sort_values("center")
+        df_high.to_csv(high_tm, index=False)
 
 
 if __name__ == "__main__":
